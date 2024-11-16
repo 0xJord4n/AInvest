595ca9e678794042ef5badce505b426c9d6b049c
0xJordan <me@0xjordan.dev>
2024-11-16 16:50:31 +0700

    style(providers.tsx): format code for consistency and readability by adjusting quotes and spacing fix(providers.tsx): change embeddedWallets.createOnLogin value from 'users-without-wallets' to 'all-users' to broaden wallet creation scope

--- webapp/src/components/providers.tsx
+++ webapp/src/components/providers.tsx
@@ -1,22 +1,22 @@
-'use client';
+"use client";
 
-import {PrivyProvider} from '@privy-io/react-auth';
+import { PrivyProvider } from "@privy-io/react-auth";
 
-export default function Providers({children}: {children: React.ReactNode}) {
+export default function Providers({ children }: { children: React.ReactNode }) {
   return (
     <PrivyProvider
       appId="cm3ivfv2c037nptk4s28yyxhv"
       config={{
         // Customize Privy's appearance in your app
         appearance: {
-          theme: 'light',
-          accentColor: '#676FFF',
-          logo: 'https://github.com/0xJord4n/AInvest/blob/main/webapp/src/public/logo/logo.png?raw=true',
+          theme: "light",
+          accentColor: "#676FFF",
+          logo: "https://github.com/0xJord4n/AInvest/blob/main/webapp/src/public/logo/logo.png?raw=true",
         },
-        loginMethods: ['email' ,'wallet', 'google', 'apple', 'twitter'],
+        loginMethods: ["email", "wallet", "google", "apple", "twitter"],
         // Create embedded wallets for users who don't have a wallet
         embeddedWallets: {
-          createOnLogin: 'users-without-wallets',
+          createOnLogin: "all-users",
         },
       }}
     >
